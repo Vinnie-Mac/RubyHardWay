91a7ed09de6975689849391c8babf8184ab690ef
Vincent McDonald <vincent.mcdonald@emishealth.com>
2016-08-16 13:33:41 +0100

    Add 13th exercise, still potching

---
+++ ex13.rb
@@ -0,0 +1,22 @@
+# Reading Files
+#
+# Getting input from the user is something that's been been done previously, so now
+# it's time to start getting data from a file. Working with files can be harsh because
+# it's easy to lose data if done incorrectly.
+#
+# We will write two files. One is the usual file to run but the other is names ex13_sample.txt
+# This second file isn't a script but a plain text file that'll be read into the script.
+
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}: "
+print txt.read
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
